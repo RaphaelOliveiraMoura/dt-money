66db26c8e235f87e12a41b8eed9728e1cec05712
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-03-20 19:44:58 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { NewTransactionModal } from './components/NewTrasactionModal';
-
+import { NewTransactionModal } from "./components/NewTrasactionModal";
 import { GlobalStyle } from "./styles/global";
-import { TransactionsProvider } from './hooks/useTransactions';
+import { TransactionsProvider } from "./hooks/useTransactions";
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
 export function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
 
   function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true)
+    setIsNewTransactionModalOpen(true);
   }
 
   function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false)
+    setIsNewTransactionModalOpen(false);
   }
 
   return (
